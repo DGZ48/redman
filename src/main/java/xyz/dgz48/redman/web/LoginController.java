78eb62feb0c3a81db0992b91fbe842eb521945b4
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-04 02:05:13 +0900

    add auth via google

--- src/main/java/xyz/dgz48/redman/web/LoginController.java
+++ src/main/java/xyz/dgz48/redman/web/LoginController.java
@@ -1,34 +1,39 @@
 package xyz.dgz48.redman.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.Map;
 
-// TODO ログインコントローラーという名前のコントローラーはいらないような気がする
-// TODO 初めてのログインの場合はユーザ登録して云々はaspectであらゆるエンドポイントに対して適用できないかなという感覚
-@RestController
+@Controller
 public class LoginController {
+
     Logger logger = LoggerFactory.getLogger(LoginController.class);
 
+    /**
+     * 認証後に表示するページ。最終的にリソースの一覧か何かになると思う
+     * @param authentication
+     * @return
+     */
     @GetMapping("/")
-    public Map<?, ?> login(Principal principal) {
-        logger.debug("access user:{}", principal.getName());
-        if (principal instanceof OAuth2AuthenticationToken) {
-            final OAuth2AuthenticationToken auth = (OAuth2AuthenticationToken) principal;
-            logger.debug("login user:{}", auth.getPrincipal().getAttributes());
+    public Map<String, Object> index(OAuth2AuthenticationToken authentication) throws JsonProcessingException {
+        return authentication.getPrincipal().getAttributes();
+    }
 
-            // TODO この値でDB参照。なければUserとして新規登録/あったら何もしない
-            String sub = String.valueOf(auth.getPrincipal().getAttributes().get("sub"));
-            // TODO ログイン後最初のページを開く（今はGoogleから取ってきたやつ出してるだけ）
-            return auth.getPrincipal().getAttributes();
-        }
-
-        logger.debug("login fail user{}", principal.getName());
-        return null;
+    /**
+     * ログイン画面表示
+     * @return
+     */
+    @GetMapping("/login")
+    public String viewLogin() {
+        return "forward:/index.html";
     }
 }
