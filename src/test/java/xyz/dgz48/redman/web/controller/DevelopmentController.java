d95766c0dbd2b407dc90587a6d801d3374089070
ishikawa <ishikawa.masayuki.0605@gmail.com>
2018-03-04 03:42:33 +0900

    [WIP]AdminLTEを適用

---
+++ src/test/java/xyz/dgz48/redman/web/controller/DevelopmentController.java
@@ -0,0 +1,26 @@
+package xyz.dgz48.redman.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 開発者目的の{@link Controller}.
+ *
+ * @author win2cot
+ *
+ */
+@Controller
+public class DevelopmentController {
+
+	/**
+	 * path:/starter.html.
+	 *
+	 * @param model モデル
+	 * @return テンプレートパス
+	 */
+	@GetMapping("/starter.html")
+	public String viewStarter(final Model model) {
+		return "/starter";
+	}
+}
