5689d06cc89a4993975891e821dba136421c40bd
ishikawa <ishikawa.masayuki.0605@gmail.com>
2018-03-09 00:44:39 +0900

    リソース登録画面、詳細画面

--- src/main/java/xyz/dgz48/redman/web/ResourceNewController.java
+++ src/main/java/xyz/dgz48/redman/web/ResourceNewController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * {@link Controller} for Resource create flow.
@@ -24,5 +25,49 @@ public class ResourceNewController {
 	@GetMapping("/input.html")
 	public String viewInput(final Model model) {
 		return "/resource/new/input";
+	}
+
+	/**
+	 * リソース登録入力画面の入力値を検証する.
+	 *
+	 * @param model モデル
+	 * @return テンプレートパス
+	 */
+	@RequestMapping(path = "/input.html", method = RequestMethod.POST)
+	public String validate(final Model model) {
+		return "redirect:/resource/new/confirm.html";
+	}
+
+	/**
+	 * リソース登録確認画面を表示する.
+	 *
+	 * @param model モデル
+	 * @return テンプレートパス
+	 */
+	@RequestMapping(path = "/confirm.html", method = RequestMethod.GET)
+	public String confirm(final Model model) {
+		return "/resource/new/confirm";
+	}
+
+	/**
+	 * リソースを登録する.
+	 *
+	 * @param model モデル
+	 * @return テンプレートパス
+	 */
+	@RequestMapping(path = "/confirm.html", method = RequestMethod.POST)
+	public String register(final Model model) {
+		return "redirect:/resource/new/complete.html";
+	}
+
+	/**
+	 * リソース登録完了画面を表示する.
+	 *
+	 * @param model モデル
+	 * @return テンプレートパス
+	 */
+	@RequestMapping(path = "/complete.html", method = RequestMethod.GET)
+	public String complete(final Model model) {
+		return "/resource/new/complete";
 	}
 }
--- src/main/java/xyz/dgz48/redman/web/ResourceViewController.java
+++ src/main/java/xyz/dgz48/redman/web/ResourceViewController.java
@@ -3,6 +3,7 @@ package xyz.dgz48.redman.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * {@link Controller} for Resource list page.
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  */
 @Controller
-public class ResourceListController {
+public class ResourceViewController {
 
 	/**
 	 * リソース一覧画面を表示する.
@@ -22,5 +23,17 @@ public class ResourceListController {
 	@GetMapping("/resource/list.html")
 	public String viewList(final Model model) {
 		return "/resource/list";
+	}
+
+	/**
+	 * リソース詳細画面を表示する.
+	 *
+	 * @param model モデル
+	 * @param id ID
+	 * @return テンプレートパス
+	 */
+	@GetMapping(path = "/resource/{id}/detail.html")
+	public String detail(final Model model, @PathVariable("id") final String id) {
+		return "/resource/detail";
 	}
 }
