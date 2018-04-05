26c9486c2601a83a1945c2224000274c7939d20f
inabajunmr <inabajun.for.regi@gmail.com>
2018-04-05 10:38:52 +0900

    test for GooleUserInfoExtractor

--- src/test/java/xyz/dgz48/redman/domain/user/userinfo/GoogleUserInfoExtractorTest.java
+++ src/test/java/xyz/dgz48/redman/domain/user/userinfo/GoogleUserInfoExtractorTest.java
@@ -2,16 +2,23 @@ package xyz.dgz48.redman.domain.user.userinfo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Instant;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.junit4.SpringRunner;
-import xyz.dgz48.redman.domain.user.IdpType;
-import xyz.dgz48.redman.domain.user.userinfo.UserInfoExtractor;
 
 
 /**
@@ -19,7 +26,7 @@ import xyz.dgz48.redman.domain.user.userinfo.UserInfoExtractor;
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class UserInfoExtractorTest {
+public class GoogleUserInfoExtractorTest {
 
 	/**
 	 * userInfo.
@@ -31,6 +38,7 @@ public class UserInfoExtractorTest {
 	 */
 	@Before
 	public void init() {
+		userInfo.put("sub", "testsub");
 		userInfo.put("email", "test@example.com");
 		userInfo.put("picture", "http://example.com/user.jpg");
 	}
@@ -41,44 +49,21 @@ public class UserInfoExtractorTest {
 	@Test
 	public void extractEmailByGoogle() {
 		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(IdpType.GOOGLE);
+		UserInfoExtractor sut = new GoogleUserInfoExtractor();
+
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+		OAuth2User oidcUser = new DefaultOidcUser(authorities, new OidcIdToken("sampletoken", Instant.MIN, Instant.MAX, userInfo));
+		OAuth2AuthenticationToken token = new OAuth2AuthenticationToken(oidcUser, authorities, "google");
 
 		// exercise
-		String actual = sut.getEmail(userInfo);
+		String actual = sut.getEmail(token);
 
 		// verify
 		assertThat(actual).isEqualTo("test@example.com");
 	}
 
-	/**
-	 * Extract email by GitHub.
-	 */
-	@Test(expected = UnsupportedOperationException.class)
-	public void extractEmailByGitHub() {
-		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(IdpType.GITHUB);
-
-		// exercise
-		String actual = sut.getEmail(userInfo);
-
-		// verify
-		assertThat(actual).isNull();
-	}
-
-	/**
-	 * Extract by Other.
-	 */
-	@Test(expected = UnsupportedOperationException.class)
-	public void extractEmailByOther() {
-		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(null);
-
-		// exercise
-		String actual = sut.getEmail(userInfo);
-
-		// verify
-		assertThat(actual).isNull();
-	}
 
 	/**
 	 * Extract picture url by Google.
@@ -86,42 +71,19 @@ public class UserInfoExtractorTest {
 	@Test
 	public void extractPictureUrlByGoogle() {
 		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(IdpType.GOOGLE);
+		UserInfoExtractor sut = new GoogleUserInfoExtractor();
+
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+		OAuth2User oidcUser = new DefaultOidcUser(authorities, new OidcIdToken("sampletoken", Instant.MIN, Instant.MAX, userInfo));
+		OAuth2AuthenticationToken token = new OAuth2AuthenticationToken(oidcUser, authorities, "google");
 
 		// exercise
-		String actual = sut.getPictureUrl(userInfo);
+		String actual = sut.getPictureUrl(token);
 
 		// verify
 		assertThat(actual).isEqualTo("http://example.com/user.jpg");
 	}
 
-	/**
-	 * Extract picture url by GitHub.
-	 */
-	@Test(expected = UnsupportedOperationException.class)
-	public void extractPictureUrlByGitHub() {
-		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(IdpType.GITHUB);
-
-		// exercise
-		String actual = sut.getPictureUrl(userInfo);
-
-		// verify
-		assertThat(actual).isNull();
-	}
-
-	/**
-	 * Extract picture url by Other.
-	 */
-	@Test(expected = UnsupportedOperationException.class)
-	public void extractPictureUrlByOther() {
-		// set up
-		UserInfoExtractor sut = new UserInfoExtractor(null);
-
-		// exercise
-		String actual = sut.getPictureUrl(userInfo);
-
-		// verify
-		assertThat(actual).isNull();
-	}
 }
